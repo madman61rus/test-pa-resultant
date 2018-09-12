28994673cbceefeddc303f421bd73752c645dd6f
Vladimir <vkompaniec@gmail.com>
2018-09-12 23:02:42 +0300

    add navigation

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './redux/store';
-//import RootStack from './src/navigation/Navigation';
+import MainCurrenciesScreen from "./modules/currencies/MainCurrenciesScreen";
+import RootStack from './navigation/Navigation';
 
 
 type Props = {};
@@ -14,31 +15,9 @@ export default class App extends Component<Props> {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text style={styles.welcome}>Welcome to React Native!</Text>
-          <Text style={styles.instructions}>To get started, edit App.js</Text>
-          <Text style={styles.instructions}>BlaBla</Text>
-        </View>
+        <RootStack />
       </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
