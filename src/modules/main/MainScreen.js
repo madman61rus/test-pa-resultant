20e32c05b5873753d2f184f37ea7348696bc581d
Vladimir Kompaniec <vkompaniec@gmail.com>
2018-09-14 09:52:45 +0300

    add text

--- src/modules/main/MainScreen.js
+++ src/modules/main/MainScreen.js
@@ -13,17 +13,17 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 class MainScreen extends Component {
 
-  render(){
+  render() {
     return (
       <View style={styles.container}>
         <View style={styles.navbar}>
           <View style={{flex: 1, paddingLeft: 10}}>
-            <TouchableOpacity onPress={() => this.props.navigation.openDrawer() }>
-              <Icon name="bars" size={15} color="#2f3542" />
+            <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+              <Icon name="bars" size={15} color="#2f3542"/>
             </TouchableOpacity>
           </View>
           <View style={{flex: 1, alignItems: 'center'}}>
-          <Text style={{color: '#2f3542', fontSize: 17, fontWeight: 'bold'}}>Test App</Text>
+            <Text style={{color: '#2f3542', fontSize: 17, fontWeight: 'bold'}}>Test App</Text>
           </View>
           <View style={{flex: 1, alignItems: 'flex-end', paddingRight: 10}}>
             <TouchableOpacity onPress={() => this.props.fetchCurrancies()}>
@@ -31,8 +31,10 @@ class MainScreen extends Component {
             </TouchableOpacity>
           </View>
         </View>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{color: '#2f3542'}}>Переход на страницу валют в боковом меню :)</Text>
+        </View>
 
-        
       </View>
     );
   }
@@ -43,7 +45,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#dfe4ea',
-    marginTop: Platform.OS !== 'ios' ? 15 : 30 
+    marginTop: Platform.OS !== 'ios' ? 15 : 30
   },
   navbar: {
     height: 50,
